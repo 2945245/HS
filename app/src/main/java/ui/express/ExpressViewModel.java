0504d82f3847ae8d121657778d1fbe45b6de495d
2945245 <87025282+2945245@users.noreply.github.com>
2022-11-09 09:19:47 +0100

    updated main layout and about page

--- app/src/main/java/ui/express/ExpressViewModel.java
+++ app/src/main/java/ui/express/ExpressViewModel.java
@@ -1,4 +1,20 @@
 package ui.express;
 
-public class ExpressViewModel {
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class ExpressViewModel extends ViewModel {
+    private final MutableLiveData<String> mText;
+
+    public ExpressViewModel() {
+        mText = new MutableLiveData<>();
+        mText.setValue("This is gallery fragment");
+    }
+
+
+
+    public LiveData<String> getText() {
+        return mText;
+    }
 }
